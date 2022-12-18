01acfee9e6a760c8c3fd2a37a41d03455cb6c382
Mohamed Hamed Fawzy <moh.hamed,elrefaey@gmail.com>
2022-12-18 16:09:04 +0200

    Update Shop.java

--- project/Shop.java
+++ project/Shop.java
@@ -40,61 +40,6 @@ public class Shop {
         return shop;
     }
 
-    public void OpenShop() {
-        frame = ShopFrame.GetInstanceGUI(); // open Frame window
-        System.out.println("Salon is Open");
-        // set semaphore for barbers and customers
-        BarberIsReady = new Semaphore(barberCount);
-        CustomerIsReady = new Semaphore(0);
-        customersQueue = new LinkedBlockingDeque<>(WeatingSeats);
-        ArrayList<Thread> threads = new ArrayList<>();
-
-        for (int i = 1; i <= barberCount; i++) {
-            threads.add(new Thread(new Barber(BarberIsReady, CustomerIsReady, customersQueue, "Barber" + i, barberCount)));
-        }
-
-        for (Thread t : threads)
-            t.start();
-
-        int numberOfCustomer = 20;
-        for (int i = 1; i <= numberOfCustomer; i++) {
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            frame.AddNewCustomer("Customer"+i);
-            System.out.println("Customer " + i + " Enter Shop");
-            Thread thread = new Thread(new Customer("Customer"+i,i));
-            thread.start();
-        }
-    }
-
-    public void CustomerEnterSop(String customerName,int Id) {
-        Id--;
-        try {
-            // check if No Seats Available enter condion and will be in weating list
-            // and waite untill be Available chair from weating seats
-            if (WeaitingSeatsAvailable.availablePermits() == 0) {
-                frame.UpdateRowWaitingList(Id,"Now Here");
-                System.out.println(customerName + " in Waeting List");
-            }
-            WeaitingSeatsAvailable.acquire();
-
-            // send signal to barber to tell him Iam Here
-            //frame.ChangeLabelText(customerName+" Enter Shop");
-            CustomerIsReady.release();
-            customersQueue.put(customerName);
-            // if no chair availble i will set in weating Area
-            if (BarberIsReady.availablePermits() == 0 && customersQueue.size() <= WeatingSeats) {
-                System.out.println(customerName + " in waeting seats");
-                frame.UpdateRowWaitingList(Id,"");
-                frame.UpdateColumnWaitingSeats(Id,"Now Here");
-            }
-            BarberIsReady.acquire();
-            WeaitingSeatsAvailable.release();
-        } catch (Exception e) {
-        }
-    }
+    
 
 }
